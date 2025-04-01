a81331dd22812e066c2fa7d1536aad28f2cbb63d
marugandev <marugandev@gmail.com>
2025-04-01 13:33:34 +0200

    FIX context for correct deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import "./index.css";
 
 import { createRoot } from "react-dom/client";
-/* import { StrictMode } from "react"; */
+import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { MoodProvider } from "./contexts/moodContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  /*   <StrictMode> */
-  <BrowserRouter>
-    <MoodProvider>
-      <App />
-    </MoodProvider>
-  </BrowserRouter>
-  /*   </StrictMode> */
+  <StrictMode>
+    <BrowserRouter>
+      <MoodProvider>
+        <App />
+      </MoodProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
