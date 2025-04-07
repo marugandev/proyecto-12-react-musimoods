d9e2343b2ed88fb3a67c390aae3a3ea34ea2db36
marugandev <marugandev@gmail.com>
2025-04-07 14:07:50 +0200

    UPDATE songs /* not-found

--- src/pages/Songs/Songs.jsx
+++ src/pages/Songs/Songs.jsx
@@ -1,15 +1,24 @@
 import "./Songs.css";
 
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { moods } from "../../data/moods";
 
 import StepBar from "../../components/atoms/StepBar/StepBar";
 import SongCard from "../../components/molecules/SongCard/SongCard";
+import { useEffect } from "react";
 
 const Songs = () => {
   const { moodName } = useParams();
+  const navigate = useNavigate();
 
   const mood = moods.find((m) => m.name === moodName);
+
+  useEffect(() => {
+    if (!mood) navigate("/*");
+  }, [mood]);
+
+  if (!mood) return null;
+
   const { color, name } = mood;
 
   return (
